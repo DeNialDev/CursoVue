6fbb033ad6e485ca9c38fc442902b94510fcf44b
mich <danielrodrigocm@hotmail.com>
2022-09-09 10:30:59 -0500

    v-if-vshow

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
- const vueDinamico = 'vue dinamico'
+ const vueDynamic = 'vue dinamico'
  const styleColor = 'color: blue'
+ const isActive = true
 </script>
 
 <template>
-	<h1>Hola {{vueDinamico}}</h1>
+	<h1>Hola {{vueDynamic}}</h1>
 	<h2 :style="styleColor">Soy azul</h2>
+	<h2 v-if="isActive">Estoy activo</h2>
+	<p v-if="!isActive">Estoy inactivo</p> 
+	<h2 v-show="isActive">Estoy activo</h2>
 </template>
 
 <style>
