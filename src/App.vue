556198f80901a2c27623bb47803c8ff8f0a9fbed
mich <danielrodrigocm@hotmail.com>
2022-09-09 14:35:10 -0500

    Emision de eventos personalizados con emmit

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,22 @@
 <script setup>
-import { ref, computed } from "vue"
-const counter = ref(0)
-const arrayNumbers = ref([])
-const increment = () => {
-	console.log("Aumentar")
-	counter.value++
-}
-const decrement = () => counter.value--
-const reset = () => counter.value = 0
-const classCounter = computed(() => {
-	if (counter.value === 0) {
-		return 'zero'
-	}
-	if (counter.value > 0) {
-		return 'positive'
-	}
-	if (counter.value < 0) {
-		return 'negative'
-	}
-	return ''
-})
-
-const searchArray = computed(() => {
-	const exist = arrayNumbers.value.find((number) => number === counter.value)
-	if (exist === 0) {
-		return true
-	}
-	return exist ? true : false
-})
-const addToArray = () => {
-	arrayNumbers.value.push(counter.value)
+import BlogPost from './components/BlogPost.vue'
+import {ref} from 'vue'
+const posts = ref([
+	{ id: 1, title: "Post 01", body: "Descrión del post 01" },
+	{ id: 2, title: "Post 02", body: "Descrión del post 02" },
+	{ id: 3, title: "Post 03" },
+]);
+const favorite = ref('')
+const setFavorite = (title) =>{
+	favorite.value = title
 }
 </script>
 
 <template>
-	<div class="container text-center mt-5	">
-
-		<h2 :class="classCounter">{{counter}}</h2>
-		<div class="btn-group">
-			<button @click="increment" class="btn btn-success">Increment</button>
-			<button @click="decrement" class="btn btn-danger">Decrement</button>
-			<button @click="reset" class="btn btn-secondary">Reset</button>
-			<button @click="addToArray" :disabled="searchArray" class="btn btn-primary ">Add</button>
-		</div>
-
-		<ul class="list-group mt-3">
-			<li class="list-group-item" v-for="(number, index) in arrayNumbers" :key="index">
-				{{number}}
-			</li>
-		</ul>
+	<div class="container">
+		<h2>Post favorito: {{favorite}}</h2>
+		<BlogPost v-for="post in posts" :key="post.title" :title="post.title" 
+		:id="post.id" :body="post.body" @setFavorite="setFavorite" 
+		/>
 	</div>
-
 </template>
-
-<style>
-.positive {
-	color: green
-}
-
-.negative {
-	color: red
-}
-
-.zero {
-	color: black
-}
-</style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,22 @@
+/* eslint-disable */ 
+<script setup>
+defineProps({
+    title: String,
+    id: Number,
+    body:{
+        type: String,
+        default: "Sin descripcion"
+    }
+}) 
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{id}} - {{title}}</h5>
+            <p>{{body}}</p>
+            <button  @click="$emit('setFavorite', title)" class="btn btn-outline-primary">Favorito</button>
+        </div>
+    </div>
+</template>
