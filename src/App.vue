ee5ec829450deeafda17338acff13d08f501d078
mich <danielrodrigocm@hotmail.com>
2022-09-09 10:41:24 -0500

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup>
- const vueDynamic = 'vue dinamico'
- const styleColor = 'color: blue'
- const isActive = true
+
+const arrayFruits = ["Apple", "Orange", "Watermelon", "Grapes"]
 </script>
 
 <template>
-	<h1>Hola {{vueDynamic}}</h1>
-	<h2 :style="styleColor">Soy azul</h2>
-	<h2 v-if="isActive">Estoy activo</h2>
-	<p v-if="!isActive">Estoy inactivo</p> 
-	<h2 v-show="isActive">Estoy activo</h2>
+	<ul>
+		<li v-for="(fruit, index) in arrayFruits"
+		:key="index">
+			{{index}} - {{fruit}}
+		</li>
+	</ul>
 </template>
 
 <style>
