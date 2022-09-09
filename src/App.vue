7a1a8568232431a7460d28e089dfd0b4bd1364b8
mich <danielrodrigocm@hotmail.com>
2022-09-09 11:44:15 -0500

    Desafio

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <script setup>
 import {ref} from "vue"
+const counter = ref(0)
 const increment = () => {
 	console.log("Aumentar")
 	counter.value++
 }
-const counter = ref(0)
+const decrement = () => counter.value--
+const reset = () => counter.value = 0
+
 
 </script>
 
 <template>
-	<h2>{{counter}}</h2>
+	<h2 :class="counter > 0 ? 'positive' : 'negative' ">{{counter}}</h2>
 	<button @click="increment">Increment</button>
+	<button @click="decrement">Decrement</button>
+	<button @click="reset">Reset</button>
 </template>
 
 <style>
-
+.positive{
+	color: green
+}
+.negative{
+	color: red
+}
 </style>
