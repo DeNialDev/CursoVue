098a579bd5e74b396a627fddbca45cacbca4c73c
mich <danielrodrigocm@hotmail.com>
2022-09-09 10:21:28 -0500

    v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
  const vueDinamico = 'vue dinamico'
+ const styleColor = 'color: blue'
 </script>
 
 <template>
 	<h1>Hola {{vueDinamico}}</h1>
+	<h2 :style="styleColor">Soy azul</h2>
 </template>
 
 <style>
