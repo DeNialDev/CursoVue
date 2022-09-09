fab3d43be5a9d78cc6a44a2d4e832c3ba80a278e
mich <danielrodrigocm@hotmail.com>
2022-09-09 10:11:49 -0500

    Interpolacion de texto

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+ const vueDinamico = 'vue dinamico'
 </script>
 
+<template>
+	<h1>Hola {{vueDinamico}}</h1>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
